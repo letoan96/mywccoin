190d938acb89e82c81959aa85093a08dfcd8920a
nhdong13 <nhatdong1301@gmail.com>
2018-04-19 16:17:17 +0700

    set tab width = 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,19 @@ require 'json'
 file = File.read "TeamData32.json"
 data = JSON.parse(file) 
 data['teams'].each do |tmp|
-	teamName = tmp['name']
-	Team.create(name: teamName)
+  teamName = tmp['name']
+  Team.create(name: teamName)
 end
 
 
 file = File.read "MatchData64.json"
 data = JSON.parse(file)
-i = 1
-data['fixtures'].each do |data|
-	team1 = Team.where(name: data['homeTeamName']).ids.first
-	team2 = Team.where(name: data['awayTeamName']).ids.first
-	date = data['date']
-	puts "#{i} #{date} #{team1} #{team2}"
-	Match.create(team_1_id: team1, team_2_id: team2, time: date)
-	i+=1
+  i = 1
+  data['fixtures'].each do |data|
+  team1 = Team.where(name: data['homeTeamName']).ids.first
+  team2 = Team.where(name: data['awayTeamName']).ids.first
+  date = data['date']
+  puts "#{i} #{date} #{team1} #{team2}"
+  Match.create(team_1_id: team1, team_2_id: team2, time: date)
+  i+=1
 end
