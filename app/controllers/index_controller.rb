4e5b95babf7dcfba8dc4d4bd001790686909099d
letoan96 <letranthetoan@gmail.com>
2018-04-20 00:14:16 +0700

    add some model relations /display upcoming matches

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,5 @@
 class IndexController < ApplicationController
   def index
-  	@matches = Match.first
+  	@matches = Match.upcoming_matches
   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,8 @@
 class Match < ApplicationRecord
 	belongs_to :team_1, :class_name => 'Team'
 	belongs_to :team_2, :class_name => 'Team'
+
+	def self.upcoming_matches
+		Match.where('time > ?', Time.now ).order('time ASC').limit(4)
+	end
 end
