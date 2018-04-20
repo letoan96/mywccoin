85b4816fcb3abbea9f0eadca0d74690a6de68bf3
letoan96 <letranthetoan@gmail.com>
2018-04-20 11:17:18 +0700

    change seed file/ move data.json to config/data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'csv'
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'data.csv'))
+csv_text = File.read(Rails.root.join('config', 'data', 'data.json'))
 # csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 input_hash =  JSON.parse(csv_text)
 stadiums = input_hash['stadiums']
 teams = input_hash['teams']
 groups = input_hash['groups']
-
+puts seed teams
 teams.each do |team|
   a = Team.create(name: team['name'], acronym: team['iso2'])
 end
-
+puts seed groups
 groups.each_pair do |key,values|
   a = Group.create(name: key)
 end
-
+puts seed matchs
 groups.each_pair do |key,values|
   values['matches'].each do |match|
     Match.create(id: match['name'], team_1_id: match['home_team'], team_2_id: match['away_team'], time: match['date'].to_time, description: "round-table #{key}" )
