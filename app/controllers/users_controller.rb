8ab1e98281510631403a9338b8f88f98a322e35f
Tung Dao <me@tungdao.com>
2018-04-24 17:10:32 +0700

    use contract from Ruby

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,40 @@
+require 'ethereum.rb'
+
 class UsersController < ApplicationController
-  def form
+  def new
     @user = User.new
     render "users/register"
   end
 
   def create
-    @user = User.new
-    @email = params[:user][:email]
-    @nickname = params[:user][:nickname]
-    @pk = params[:user][:private_key]
-    @saveUser = User.new(email: @email,nickname: @nickname, password: "123")
-    @saveUser.save
+    @email = user_params[:email]
+    @nickname = params[:nickname]
+    @pk = params[:private_key]
+
+    config = Rails.application.config_for(:blockchain)
+    client = Ethereum::HttpClient.new(config['rpc_url'])
+    formatter = Ethereum::Formatter.new
+
+    from = formatter.to_address(config['address'])
+    to = formatter.to_address(user_params[:wallet_address])
+
+    value = client.int_to_hex(formatter.to_wei(2))
+    client.eth_send_transaction(from: from, to: to, value: value)
+
+    byebug
+
+    @user = User.create(email: @email,nickname: @nickname, password: @email)
+
     UserMailer.with(user: @nickname, mail: @email).welcome_email.deliver_later
+
     respond_to do |format|
       format.js
     end
-
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :nickname, :private_key, :wallet_address)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root 'index#index'
-  
-  match '/how-to-bet', to: 'index#how_to_bet', via: [:get, :post]
+
+  resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
-  	get '/login', to: '/clearance/sessions#new', as: 'login'
-  	post '/login', to: '/clearance/sessions#create'
-  	get '/index', to: 'admins#index', as: 'index'
-  	delete '/logout', to: '/clearance/sessions#destroy', as: 'logout'
+    get '/login', to: '/clearance/sessions#new', as: 'login'
+    post '/login', to: '/clearance/sessions#create'
+    get '/index', to: 'admins#index', as: 'index'
+    delete '/logout', to: '/clearance/sessions#destroy', as: 'logout'
   end
 
   get '/form', to: 'users#form', as: 'form'
